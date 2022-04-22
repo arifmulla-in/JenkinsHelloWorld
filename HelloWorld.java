f89d68b918f11a563c59253c5b5b27106c7051eb
arifmulla-in <73012036+arifmulla-in@users.noreply.github.com>
2022-04-22 12:27:48 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 
-		System.out.println("##############************ I am in Traning **********#######################");
+		System.out.println("###########************$$ I am in Traning $$**********#########");
 
 	}
 
